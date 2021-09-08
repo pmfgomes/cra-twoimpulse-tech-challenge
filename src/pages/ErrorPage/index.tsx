8b4e2b5fd2b36ba9bbb07a5ca25b29ce71fa2044
Pedro Gomes <pmfgomes91@gmail.com>
2021-09-08 22:30:34 +0100

    Refactor error page

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,24 +1,22 @@
-import { Col, Button, Row, Title } from "components/Layout";
-import { errorPageContainer, messageContainer, subTitle } from "./styles";
+import {
+  ButtonContainer,
+  Container,
+  HomepageLink,
+  MessageContainer,
+  subTitle,
+  title,
+} from "./styles";
 
 export default function ErrorPage(): React.ReactElement {
-  const handleRedirect = (url: string) => {
-    window.location.href = url;
-  };
-
   return (
-    <div css={errorPageContainer}>
-      <Row direction="column" justify="center" align="center">
-        <Col css={messageContainer}>
-          <Title size={160}>OOPS!</Title>
-          <h2 css={subTitle}>404 - PAGE NOT FOUND</h2>
-        </Col>
-        <Col>
-          <Button color="primary" onClick={() => handleRedirect("/")}>
-            GO TO HOMEPAGE
-          </Button>
-        </Col>
-      </Row>
-    </div>
+    <Container>
+      <MessageContainer>
+        <h1 css={title}>OOPS!</h1>
+        <h2 css={subTitle}>PAGE NOT FOUND</h2>
+      </MessageContainer>
+      <ButtonContainer>
+        <HomepageLink to="/">GO TO HOMEPAGE</HomepageLink>
+      </ButtonContainer>
+    </Container>
   );
 }
--- src/pages/ErrorPage/styles.ts
+++ src/pages/ErrorPage/styles.ts
@@ -1,14 +1,23 @@
 import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
 
-export const errorPageContainer = css`
+export const Container = styled.div`
   position: absolute;
   top: 128px;
   width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
-export const messageContainer = css`
+export const MessageContainer = styled.div`
   position: relative;
-  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   h1,
   h2 {
@@ -16,9 +25,33 @@ export const messageContainer = css`
   }
 `;
 
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const HomepageLink = styled(Link)`
+  text-decoration: none;
+  background-color: ${props => props.theme.palette.primary};
+  color: #fff;
+  border-radius: 32px;
+  padding: 8px 16px;
+  transition: opacity 100ms cubic-bezier(0.645, 0.045, 0.355, 1);
+  box-shadow: #000 0px 1px 3px;
+
+  &:active {
+    opacity: 0.8;
+  }
+`;
+
+export const title = css`
+  font-size: 160px;
+`;
+
 export const subTitle = css`
   position: absolute;
   background-color: #fff;
   bottom: 15%;
-  left: 22%;
+  left: 30%;
 `;
