370211d99f9c7d391e46806a774381d52303ac7b
Pedro Gomes <pmfgomes91@gmail.com>
2021-09-11 15:48:08 +0100

    New/update types

--- src/types/components/header.d.ts
+++ src/types/components/header.d.ts
@@ -1,7 +1,10 @@
+import { DetailedHTMLProps, HTMLAttributes } from "react";
 import { NavLinkProps } from "react-router-dom";
 
-interface IHeader {
+interface HeaderProps extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
   color?: string;
+  moreLinkItems?: React.ReactNode[];
+  navProps?: DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>;
 }
 
 interface IHeaderNav extends NavLinkProps {
---
+++ src/types/components/wrapper.d.ts
@@ -0,0 +1,6 @@
+import { HeaderProps } from "./header";
+
+interface WrapperProps {
+  children?: React.ReactNode;
+  headerProps?: HeaderProps;
+}
