94c224ffbe9c2bf43fba6051226e467b11087ada
Pedro Gomes <pmfgomes91@gmail.com>
2021-09-12 00:17:42 +0100

    Employees page with images

---
+++ src/pages/Employees/employees.styles.ts
@@ -0,0 +1,108 @@
+import styled from "@emotion/styled";
+import { HeaderLink } from "components/Header/header.styles";
+import { darken } from "polished";
+import { ReactComponent as SquareSvg } from "assets/images/el-1.svg";
+
+export const HeaderButtonLink = styled(HeaderLink)`
+  border-radius: 32px;
+  padding: 16px 32px;
+  background-color: #fff;
+  color: ${({ theme }) => theme.palette.primary};
+  transition: background-color 100ms cubic-bezier(0.645, 0.045, 0.355, 1);
+
+  &:active {
+    background-color: ${darken(0.1, "#fff")};
+  }
+`;
+
+export const EmployeesContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  box-sizing: border-box;
+
+  padding: 0 10%;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const EmployeeTitleContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 10% 0;
+
+  color: ${({ theme }) => theme.palette.primary};
+  font-size: 40px;
+  font-weight: 600;
+  width: 100%;
+  height: 100%;
+
+  &::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-image: url("/assets/images/el-1.svg");
+    background-size: 77px;
+    background-repeat: no-repeat;
+
+    z-index: -1;
+  }
+`;
+
+export const EmployeeTitle = styled.h1`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+  color: ${({ theme }) => theme.palette.primary};
+  font-size: 40px;
+  font-weight: 600;
+  width: 100%;
+  height: 100%;
+`;
+
+export const EmployeeTitleRightSquareContainer = styled.div`
+  display: grid;
+  justify-content: flex-end;
+  width: 100%;
+`;
+
+export const RightSquare = styled(SquareSvg)`
+  width: 100%;
+
+  transform: rotate(220deg);
+  z-index: -1;
+`;
+
+export const EmployeeFooter = styled.div`
+  box-sizing: border-box;
+  padding: 10% 0;
+  width: 100%;
+  height: 100%;
+`;
+
+export const FooterPinkSquare = styled(SquareSvg)`
+  width: 100%;
+  transform: translateX(-35%);
+
+  z-index: -1;
+
+  rect {
+    fill: #feafcb;
+  }
+`;
+
+export const EmployeeCardsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+
+  width: 100%;
+  height: 100%;
+`;
--- src/pages/Employees/index.tsx
+++ src/pages/Employees/index.tsx
@@ -1,5 +1,85 @@
+import { css } from "@emotion/react";
 import Wrapper from "components/Wrapper";
+import { useEffect, useState } from "react";
+import useStore from "store";
+import {
+  EmployeeTitleContainer,
+  EmployeesContainer,
+  HeaderButtonLink,
+  EmployeeTitle,
+  RightSquare,
+  EmployeeTitleRightSquareContainer,
+  EmployeeFooter,
+  FooterPinkSquare,
+  EmployeeCardsContainer,
+} from "./employees.styles";
+import EMPLOYEE_DATA from "mocks/employeeData";
+import EmployeeCard from "components/EmployeeCard";
 
 export default function Employees(): React.ReactElement {
-  return <Wrapper></Wrapper>;
+  const employeesList = useStore(store => store.employeesList);
+  const setEmployeesList = useStore(store => store.setEmployeesList);
+
+  const [loadingData, setLoadingData] = useState(true);
+
+  const moreLinkItems = [
+    <HeaderButtonLink key="new-employee-button-link" to="/employees">
+      New Employee
+    </HeaderButtonLink>,
+  ];
+
+  const getEmployeeData = () => {
+    setTimeout(() => {
+      setEmployeesList(EMPLOYEE_DATA);
+      setLoadingData(false);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    getEmployeeData();
+  }, []);
+
+  const content = () => {
+    if (loadingData) {
+      return <div>Loading data...</div>;
+    }
+
+    const isEmployeesListLengthOdd =
+      employeesList && employeesList.length > 0 && !!(employeesList.length & 1);
+
+    return (
+      <EmployeeCardsContainer>
+        {employeesList?.map(employee => (
+          <EmployeeCard key={`employee-card-${employee.id}`} data={employee} />
+        ))}
+        {isEmployeesListLengthOdd && <EmployeeCard visible={false} />}
+      </EmployeeCardsContainer>
+    );
+  };
+
+  return (
+    <Wrapper
+      headerProps={{
+        moreLinkItems,
+        navProps: {
+          css: css`
+            padding-right: 64px;
+          `,
+        },
+      }}
+    >
+      <EmployeesContainer>
+        <EmployeeTitleContainer>
+          <EmployeeTitle>Team</EmployeeTitle>
+          <EmployeeTitleRightSquareContainer>
+            <RightSquare />
+          </EmployeeTitleRightSquareContainer>
+        </EmployeeTitleContainer>
+        {content()}
+        <EmployeeFooter>
+          <FooterPinkSquare />
+        </EmployeeFooter>
+      </EmployeesContainer>
+    </Wrapper>
+  );
 }
