00780e4a155339d645582c923979e344db39d9e9
Pedro Gomes <pmfgomes91@gmail.com>
2021-09-12 00:17:10 +0100

    Remove console.log

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,9 +3,6 @@ export const getRandomInt = max => {
 };
 
 export const randomIsoDate = (start, end) => {
-  console.log("file: helpers.ts ~ line 6 ~ randomIsoDate ~ start", start);
-  console.log("file: helpers.ts ~ line 6 ~ randomIsoDate ~ end", end);
-
   return new Date(
     start.getTime() + Math.random() * (end.getTime() - start.getTime()),
   ).toISOString();
