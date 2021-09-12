c130b26449122a5c27703e19fb232fe9ff6c972c
Pedro Gomes <pmfgomes91@gmail.com>
2021-09-12 18:26:59 +0100

    Refactor component for datepicker

--- src/components/FormCardInput/index.tsx
+++ src/components/FormCardInput/index.tsx
@@ -1,25 +1,56 @@
-import { Field } from "formik";
+import { useFormikContext } from "formik";
 import { FormInputContainer, FormValue } from "./formCardInput.styles";
 import dayjs from "dayjs";
+import { DatePicker } from "react-dayjs-picker";
+import { useState } from "react";
+
+const DATE_FORMAT = "DD/MM/YYYY";
 
 export default function FormCardInput(props: FormCardInputProps): React.ReactElement {
   const { label, name, type = "text", edit = false, containerProps } = props;
+
+  const formik = useFormikContext();
+  const field = formik.getFieldProps(name);
+
+  const [openPicker, setOpenPicker] = useState(false);
+
+  const selectPicker = () => {
+    if (!edit) {
+      let newValue = field.value;
+      if (type === "date") {
+        newValue = field.value ? dayjs(field.value).format(DATE_FORMAT) : undefined;
+      }
+
+      return <FormValue>{newValue}</FormValue>;
+    }
+
+    if (type === "date") {
+      return (
+        <DatePicker
+          format={DATE_FORMAT}
+          isOpen={openPicker}
+          setIsOpen={setOpenPicker}
+          closeOnSelect
+          markToday
+          popoverPositions={["bottom"]}
+          date={field.value ? dayjs(field.value) : undefined}
+          onSelect={date => {
+            formik.setFieldValue(name!, date.toISOString());
+          }}
+          onChange={() => {
+            return;
+          }}
+        />
+      );
+    }
+
+    return <input name={name} id={name} value={field.value} onChange={field.onChange} />;
+  };
+
   return (
     <FormInputContainer {...containerProps}>
       <label htmlFor={name}>{label}</label>
-      <Field name={name}>
-        {({ field }) => {
-          let newValue = field.value;
-          if (type === "date" && field.value) {
-            newValue = dayjs(field.value).format("DD/MM/YYYY HH:mm:ss");
-          }
-          if (!edit) {
-            return <FormValue>{newValue}</FormValue>;
-          }
-
-          return <input name={name} id={name} value={newValue} onChange={field.onChange} />;
-        }}
-      </Field>
+      {selectPicker()}
     </FormInputContainer>
   );
 }
