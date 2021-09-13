3e2e9c29c4a2685abf5d78749f7824e637dc722f
Pedro Gomes <pmfgomes91@gmail.com>
2021-09-13 01:22:06 +0100

    Put data on store

--- src/pages/EmployeesPage/Employees.tsx
+++ src/pages/EmployeesPage/Employees.tsx
@@ -1,36 +1,12 @@
-import { useEffect, useState } from "react";
 import useStore from "store";
 import { EmployeeFooter, FooterPinkSquare, EmployeeCardsContainer } from "./employeesPage.styles";
-import EMPLOYEE_DATA from "mocks/employeeData";
 import EmployeeCard from "components/EmployeeCard";
 import EmployeePageTitle from "./EmployeePageTitle";
 
 export default function Employees(): React.ReactElement {
   const employeesList = useStore(store => store.employeesList);
-  const setEmployeesList = useStore(store => store.setEmployeesList);
-
-  const [loadingData, setLoadingData] = useState(true);
-
-  const getEmployeeData = () => {
-    setTimeout(() => {
-      setEmployeesList(EMPLOYEE_DATA);
-      setLoadingData(false);
-    }, 500);
-  };
-
-  useEffect(() => {
-    if (!employeesList) {
-      getEmployeeData();
-    } else {
-      setLoadingData(false);
-    }
-  }, []);
 
   const content = () => {
-    if (loadingData) {
-      return <div>Loading data...</div>;
-    }
-
     const isEmployeesListLengthOdd =
       employeesList && employeesList.length > 0 && !!(employeesList.length & 1);
 
